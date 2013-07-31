3def6899e5034eebdd94012735fa69344f880eed
mingshi <fivemingshi@gmail.com>
2013-07-31 20:48:48 +0800

    add department edit

--- member/views/department.py
+++ member/views/department.py
@@ -45,3 +45,51 @@ def do_add() :
         return json.dumps(result)
     else :
         return redirect('/403')
+
+@mod.route('/department/del-<id>', methods=['GET','POST'])
+def del_department(id) :
+    if check_admin() :
+        result = {}
+        id = int(id)
+        _department = db_session.query(Department).filter_by(id = id).first()
+        db_session.delete(_department)
+        db_session.commit()
+        result['code'] = 0
+        result['msg'] = 'del success'
+        return json.dumps(result)
+    else :
+        return redirect('/403')
+
+@mod.route('/department/edit-<id>')
+def edit_department(id) :
+    if check_admin() :
+        _department = db_session.query(Department).filter_by(id = id).first()
+        return render_template('member/edit_department.html', department = _department)
+    else :
+        return redirect('/403')
+
+@mod.route('/department/do_edit', methods=['POST'])
+def do_edit_department() :
+    if check_admin() :
+        result = {}
+        if not request.form.has_key('id') :
+            result['code'] = 101
+            result['msg'] = 'id must be required.'
+        elif not request.form.has_key('name') :
+            result['code'] = 102
+            result['msg'] = 'name must be required.'
+        else :
+            name = request.form['name'].strip().encode('utf8')
+            id = int(request.form['id'].strip())
+            _department = db_session.query(Department).filter_by(id = id).first()
+            if _department :
+                _department.name = name
+                db_session.commit()
+                result['code'] = 0
+                result['msg'] = 'edit success.'
+            else :
+                result['code'] = 103
+                result['msg'] = 'invalid id.'
+        return json.dumps(result)
+    else :
+        return redirect('/403')
