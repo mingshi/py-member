d3ba01202b2a43ccde07cde596adba9aa1e958bf
mingshi <fivemingshi@gmail.com>
2013-08-12 17:39:20 +0800

    add uwsgi

--- run.py
+++ run.py
@@ -6,4 +6,5 @@ if __name__ == "__main__":
     if app.config['DEBUG'] :
         print app.config
 
-    app.run(host=app.config['HOST'], port=app.config['PORT'])
+    #app.run(host=app.config['HOST'], port=app.config['PORT'])
+    app.run()
